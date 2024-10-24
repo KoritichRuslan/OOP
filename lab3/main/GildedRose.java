59778c46b5ff9a8b646dbba4565fec3b4fc33ed7
KoritichRuslan <154692840+KoritichRuslan@users.noreply.github.com>
2024-10-24 21:17:46 +0300

    the final look of GildedRose

--- lab3/main/GildedRose.java
+++ lab3/main/GildedRose.java
@@ -9,37 +9,7 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            switch (item.getName()) {
-                case "Aged Brie":
-                    Item.increaseQualityByOne(item);
-                    item.setSellIn(item.getSellIn() - 1);
-                    if (item.getSellIn() < 0) {
-                        Item.increaseQualityByOne(item);
-                    }
-                    break;
-                case "Backstage passes to a TAFKAL80ETC concert":
-                    Item.increaseQualityByOne(item);
-                    if (item.getSellIn() < 6) {
-                        Item.increaseQualityByOne(item);
-                    }
-                    if (item.getSellIn() < 11) {
-                        Item.increaseQualityByOne(item);
-                    }
-                    item.setSellIn(item.getSellIn() - 1);
-                    if (item.getSellIn() < 0) {
-                        item.setQuality(0);
-                    }
-                    break;
-                case "Sulfuras, Hand of Ragnaros":
-                    break;
-                default:
-                    Item.decreaseQualityByOne(item);
-                    item.setSellIn(item.getSellIn() - 1);
-                    if (item.getSellIn() < 0) {
-                        Item.decreaseQualityByOne(item);
-                    }
-                    break;
-            }
+            item.updateQuality();
         }
     }
 }
