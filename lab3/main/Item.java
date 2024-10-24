039c8c8f434d4b99d30940972b1611fb5ae35373
KoritichRuslan <154692840+KoritichRuslan@users.noreply.github.com>
2024-10-24 20:21:17 +0300

    fix errors
    at this stage, I realized that it would be necessary to rewrite some methods of the Item class in other classes (@Override). so I changed the structure a bit - otherwise it will result in an error

--- lab3/main/Item.java
+++ lab3/main/Item.java
@@ -1,12 +1,9 @@
 package com.gildedrose;
-
 public class Item {
 
-    private String name;
-
-    private int sellIn;
-
-    private int quality;
+    public String name;
+    public int sellIn;
+    public int quality;
 
     public Item(String name, int sellIn, int quality) {
         this.name = name;
@@ -14,34 +11,30 @@ public class Item {
         this.quality = quality;
     }
 
-    public static void increaseQualityByOne(Item item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
+    public void increaseQualityByOne() {
+        if (getQuality() < 50) {
+            setQuality(getQuality() + 1);
         }
     }
 
-    public static void decreaseQualityByOne(Item item) {
-        if (item.getQuality() > 0) {
-            item.setQuality(item.getQuality() - 1);
+    public void decreaseQualityByOne() {
+        if (getQuality() > 0) {
+            setQuality(getQuality() - 1);
         }
     }
 
-    static void updateQuality(Item item) {
-        decreaseQualityByOne(item);
-        item.setSellIn(item.getSellIn() - 1);
-        if (item.getSellIn() < 0) {
-            decreaseQualityByOne(item);
+    public void updateQuality() {
+        decreaseQualityByOne();
+        setSellIn(getSellIn() - 1);
+        if (getSellIn() < 0) {
+            decreaseQualityByOne();
         }
     }
 
     public void setSellIn(int sellIn) { this.sellIn = sellIn; }
-
     public void setQuality(int quality) { this.quality = quality; }
-
     public String getName() { return name; }
-
     public int getSellIn() { return sellIn; }
-
     public int getQuality() { return quality; }
 
     @Override
